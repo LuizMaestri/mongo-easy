35dc29858ad363797d225d380dbb91f57c652d40
luiz.maestri <luiz.maestri@neoway.com.br>
2017-01-09 16:27:37 -0200

    continue implementing Document init method

--- mongo_easy/document.py
+++ mongo_easy/document.py
@@ -3,20 +3,32 @@ from mongo_easy.field import DictField, Field
 
 class Document(dict):
 
-    STRUCTURE = DictField()
+    __database__ = ''
+    __structure__ = DictField()
 
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        struct = self.STRUCTURE
-        print(struct)
+        super().__init__()
+        for key in [k for k, v in self.__structure__.items() if v.not_none]:
+            value = kwargs.get(key, None)
+            if value is None:
+                
+            if key in kwargs:
+                del kwargs[key]
+        for k, v in kwargs.items():
+            self[k] = v
+
+        def __setitem__(self, key, value):
+            self.__structure__[key] = value
+            super().__setitem__(key, value)
+
 
 class User(Document):
 
-    STRUCTURE = DictField(
-        name=Field(str),
-        username=Field(str),
-        password=Field(str),
-        son=Field(Document)
+    __structure__ = DictField(
+        name=Field(str, default=''),
+        username=Field(str, not_none=False),
+        password=Field(str, not_none=False),
+        son=Field(Document, not_none=False)
     )
 
-print(User(son=User(name='luiz')))
+print(User(name='ká', son=User(name='luiz')))
